e86cb34c2fc67fc8a731d70e17a08d5ef1f4cce5
Michael Redford <michaeliredford@gmail.com>
2018-03-25 18:57:11 +0900

    AppImage Support

---
+++ gen/AppImage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+clear
+cd .. && mkdir build/
+cd build/
+cmake .. && cmake --build .
+chmod a+x ariel
+cd ..
+#care -o AppDir/ build/ariel
+#mv -v AppDir/rootfs/* AppDir && rm -r -v AppDir/rootfs  && rm -r -v AppDir/home
+mkdir -p AppDir/usr/bin/ && mkdir AppDir/usr/lib
+mkdir -p AppDir/share/applications && mkdir -p AppDir/share/icons/highcolor/256x256
+cp AppDir/ariel.desktop AppDir/share/applications && cp AppDir/ariel.png AppDir/share/icons/highcolor/256x256
+cp build/ariel AppDir/
+read -rsp $'Press any key to continue...\n' -n 1 key
+wget -nc https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
+chmod a+x linuxdeployqt*.AppImage
+./linuxdeployqt*.AppImage AppDir/ariel.desktop -appimage
+chmod a+x Ariel_Engine-*.AppImage
+echo -e "running generated AppImage"
+./Ariel_Engine-x86_64.AppImage
+
+#clean up
+cd AppDir/ && rm -R -f -- */ && cd ..
+#rm -v appimagetool*.AppImage 
+rm -r  build
