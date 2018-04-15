28b6decb0618b6d760c87a4f652c3c04f589dbdc
Michael <michael.redford@live.com>
2018-04-15 22:38:41 +0900

    fixed windows compilation errors

--- gen/Windows-Build.bat
+++ gen/Windows-Build.bat
@@ -1,7 +1,9 @@
+clear
+cd ..
 mkdir build
 cd build
 cmake .. -DCMAKE_GENERATOR_PLATFORM=x64
-cmake --build .  
+cmake --build .
 copy Debug\ariel.exe ..\bin\ariel.exe
 cd ..
 del /S /Q build
---
+++ src/rect.hpp
@@ -0,0 +1,8 @@
+
+class Rect {
+        int x, y, w, h;
+    public:
+        void SetPosition(int x, int y);
+        Rect GetRect();
+        void SetSize(int h, int w);
+};
